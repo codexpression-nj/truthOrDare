f46430d85ee7656d0992a579a3153162d86a438f
codexpression-nj <n.codexpression@gmail.com>
2023-02-28 23:28:31 +0200

    daily update

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Request from './components/Request';
+import { NavigationContainer } from '@react-navigation/native';
+import AddFriends from './components/AddFriends';
+import Categories from './components/Categories';
+import Results from './components/Results';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+    <Stack.Screen name="AddFriends" component={AddFriends} />
+    <Stack.Screen name="Categories" component={Categories} />
+    <Stack.Screen name="Request" component={Request} />
+    <Stack.Screen name="Results" component={Results} />
+
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
