9a6eb7a93e9b3fa5d81e2cfb94f6a58b8dbc6054
codexpression-nj <n.codexpression@gmail.com>
2023-03-11 23:44:11 +0200

    update

--- components/Request.js
+++ components/Request.js
@@ -1,21 +1,50 @@
 //import liraries
 import React, { Component, useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, SafeAreaView } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 // create a component
-const Request = ({navigation,route}) => {
+const Request = ({ navigation, route }) => {
     const type = route.params
     console.log(type.opt);
     return (
-        <View style={styles.container}>
-            <Text>Request</Text>
-            <TouchableOpacity 
-                onPress={() => navigation.navigate('Results')  }
-            ><Text>Done</Text></TouchableOpacity>
-            <TouchableOpacity
-                onPress={() => navigation.navigate('Results')  }
-            ><Text>Give up</Text></TouchableOpacity>
-        </View>
+        <SafeAreaView style={styles.container}>
+            {type.opt == 'truth'
+                ?<LinearGradient
+                    // Background Linear Gradient
+                    // colors ={ type.opt ? 'magenta' : ['#019CF5', '#016BF7']}
+
+                    colors={['#019CF5', '#016BF7']}
+                    style={styles.backg}
+                >
+                    <Text>Request</Text>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('Results')}
+                    ><Text>Done</Text></TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('Results')}
+                    ><Text>Give up</Text></TouchableOpacity>
+                </LinearGradient>
+                : <LinearGradient
+                    // Background Linear Gradient
+                    // colors ={ type.opt ? 'magenta' : ['#019CF5', '#016BF7']}
+
+                    colors={['#FA696A', '#EB5856']}
+                    style={styles.backg}
+                >
+                    <Text>Request</Text>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('Results')}
+                    ><Text>Done</Text></TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('Results')}
+                    ><Text>Give up</Text></TouchableOpacity>
+                </LinearGradient>
+            }
+        </SafeAreaView >
+
+
+
     );
 };
 
@@ -23,10 +52,16 @@ const Request = ({navigation,route}) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        // justifyContent: 'center',
+        // alignItems: 'center',
+    },
+    backg:{
+        flex: 1,
+        // width: '100%',
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#0092F5',
-    },
+        // objectFit:'cover'
+    }
 
 });
 
